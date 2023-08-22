d4f0e0fb50edcfe79a3bf42568b299ea0bc903cd
Adebayo Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-08-22 23:53:39 +0100

    Update shell.h

--- Tests/shell.h
+++ Tests/shell.h
@@ -15,5 +15,14 @@ int count_token(char *input);
 int executes_commands(char **av);
 void handle_child_process(pid_t pidv, char **av);
 void process_input(char *buff);
+char *_strcat(char *dest, const char *src);
+char *_strcpy(char *dest, const char *src);
+size_t _strlen(const char *s);
+int _strcmp(char *s1, char *s2);
+char *_strdup(char *str);
+void _memcpy(void *dest, const void *src, unsigned int size);
+char *_getenv(const char *name);
+int _strncmp(const char *s1, const char *s2, size_t n);
+
 
 #endif
