35698359eaab96fc6c732365e34898cf05c8ca44
Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-08-22 11:20:27 -0700

    testing

---
+++ Tests/function_1.c
@@ -0,0 +1,69 @@
+#include "shell.h"
+
+int contains_path(char *exec) {
+	for (int q = 0; exec[q] != '\0'; q++) {
+		if (exec[q] == '/') {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+char *get_full_path(char *exec, char *path_token) {
+	char *fullpath = malloc(strlen(path_token) + strlen(exec) + 2);
+	if (!fullpath) {
+		perror("Memory Allocation failed");
+		exit(EXIT_FAILURE);
+	}
+	strcpy(fullpath, path_token);
+	strcat(fullpath, "/");
+	strcat(fullpath, exec);
+	return fullpath;
+}
+
+char *search_executable_in_path(char *exec, char *path_value) {
+	char *path_copy = strdup(path_value);
+	char *path_token = strtok(path_copy, ":");
+	char *fullpath = NULL;
+
+	while (path_token != NULL) {
+		fullpath = get_full_path(exec, path_token);
+		struct stat stv;
+		if (stat(fullpath, &stv) == 0) {
+			break;
+		}
+		free(fullpath);
+		fullpath = NULL;
+		path_token = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return fullpath;
+}
+
+int executes_commands(char **av) {
+	char *exec = av[0];
+
+	if (contains_path(exec)) {
+		if (execve(exec, av, NULL) == -1) {
+			perror(exec);
+			exit(EXIT_FAILURE);
+		}
+	} else {
+		char *path_value = getenv("PATH");
+		char *fullpath = search_executable_in_path(exec, path_value);
+
+		if (fullpath) {
+			if (execve(fullpath, av, NULL) == -1) {
+				perror(fullpath);
+				exit(EXIT_FAILURE);
+			}
+			free(fullpath);
+		} else {
+			perror(exec);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	return 0;
+}
