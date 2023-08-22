6b3b4320b38182d07a8487c9866a1c303d9f7977
Ifeoluwa <ifeoluwa.adebayo2003@gmail.com>
2023-08-21 23:57:21 -0700

    Creating shell

--- main.c
+++ main.c
@@ -7,62 +7,83 @@
  *
  * Return: (0) on success
  */
-
-int main(int ac, char *av[])
-{
+int main(int ac, char *av[]) {
 	char *buff = NULL, *buffCopy = NULL;
 	size_t length = 0;
+	int Newline = 0;
 	(void)ac;
 
 	while (1)
 	{
-		display_Prompt();
+		if (isatty(STDIN_FILENO))
+		{
+			if (Newline)
+			{
+				write(STDOUT_FILENO, "\n", sizeof("\n") - 1);
+			}
+			display_Prompt();
+		}
+		else
+		{
+			if (Newline)
+				write(STDOUT_FILENO, "\n", sizeof("\n") - 1);
+		}
+
 		ssize_t readline = getline(&buff, &length, stdin);
+
 		if (readline == EOF)
 		{
 			break;
 		}
-		buff[readline - 1] = '\0';
+
+		buff[strlen(buff) - 1] = '\0';
 		buffCopy = strdup(buff);
 
-		if (strcmp(buff, "exit") == 0)
+		if (!buffCopy) {
+			perror("Memory Allocation failed");
+			return -1;
+		}
+
+		if (strcmp(buffCopy, "exit") == 0)
 		{
 			free(buffCopy);
-			break;
+			break;  /* Exit the loop when the user enters "exit" */
 		}
-		if (!buffCopy)
-		{
-			perror("Memory Allocation failed");
-			return (-1);
-		}
+
 		int Count_Token = count_token(buffCopy);
 		char **av = malloc(sizeof(char *) * (Count_Token + 1));
 
 		if (!av)
 		{
 			perror("Memory Allocation failed");
-			return (-1);
+			return -1;
 		}
+
 		Tokenize_Input(buff, av, Count_Token);
 		pid_t pidv = fork();
 
 		if (pidv < 0)
+		{
 			perror("Error creating child process");
-		else if (pidv == 0)
+		} else if (pidv == 0)
 		{
 			executes_commands(av);
-
 			exit(EXIT_FAILURE);
-		}
-		else
+		} else
 		{
 			int status;
 			if (wait(&status) == -1)
+			{
 				perror("Error waiting for child process");
+			}
 		}
+
 		free(av);
 		free(buffCopy);
+		display_Prompt();
+		Newline = 1;
 	}
+
 	free(buff);
 	exit(0);
 }
